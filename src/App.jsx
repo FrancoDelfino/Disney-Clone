56077b5088158a17633ff4985a25aa99a70f4271
Franco Delfino <francogiovannidelfino@gmail.com>
2025-02-25 12:42:24 -0300

    Completo Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
-
+import fondo from "./assets/Images/bg-color.jpg";
 
 function App() {
 
   return (
-   <div className=''>
+   <div className='' style={{ backgroundImage: `url(${fondo})` }}>
       <Header/>
          <Slider/>
    </div>
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,32 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getTrendingMovies } from "../Services/trendingMoviesService";
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
+const screenWidth = window.innerWidth
 
 const Slider = () => {
-    const [movieList, setMovieList] = useState([])
-    
-    useEffect(() => {
-        const fetchMovies = async () => {
-          try {
-            const data = await getTrendingMovies();
-            setMovieList(data)
-          } catch (error) {
-            console.error("Error fetching movies:", error);
-          }
-        };
-    
-        fetchMovies();
-      }, []);
-     
+  const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef();
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const data = await getTrendingMovies();
+        setMovieList(data)
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  const sliderRight = (element) => {
+    element.scrollLeft-=screenWidth-109
+  }
+  const sliderLeft = (element) => {
+    element.scrollLeft+=screenWidth-109
+  }
+
+
   return (
-    
-    <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide'>
-      {movieList.map((item) => (
-        <img key={item.id} src={IMAGE_BASE_URL+item.backdrop_path} alt={item.title} className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md scrollbar-none ' />
-      ))} 
+    <div className='relative'>
+      <HiChevronLeft className='hidden md:block text-white text-[30px] absolute mx-8 cursor-pointer h-[100%]' onClick={() => sliderRight(elementRef.current)}/>
+      <HiChevronRight className='hidden md:block text-white text-[30px] absolute mx-8 right-0  cursor-pointer h-[100%]' onClick={() => sliderLeft(elementRef.current)}/>
+      <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth ' ref={elementRef}>
+        {movieList.map((item) => (
+          <img key={item.id} src={IMAGE_BASE_URL + item.backdrop_path} alt={item.title} className='min-w-full h-[350px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-300 transition-all duration-75 ease-in shadow-[0_10px_7px_rgba(0,0,0,0.2)]' />
+        ))}
+      </div>
+
     </div>
-    
   )
 }
 
